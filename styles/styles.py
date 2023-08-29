980b0c1223140b54de940072aa29c461d09e9d5c
AESMatias <119653204+AESMatias@users.noreply.github.com>
2023-08-29 18:30:58 -0400

    testing

---
+++ styles/styles.py
@@ -0,0 +1,8 @@
+button_style = """
+    background: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1,
+                                    stop: 0 #97ABFF, stop: 1 #123597);
+"""
+global_style = """
+    background: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1,
+                                    stop: 0 #6B73FF, stop: 1 #000DFF);
+"""
