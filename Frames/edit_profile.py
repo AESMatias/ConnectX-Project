3bcb5276fd0826c91473a17092b4b23eb8ef567b
AESMatias <119653204+AESMatias@users.noreply.github.com>
2023-09-05 09:59:16 -0300

    Testing menu
    new buttons in the profile edit window
    
    Co-Authored-By: eMonkey <113893889+emonkey0@users.noreply.github.com>

--- Frames/edit_profile.py
+++ Frames/edit_profile.py
@@ -24,37 +24,75 @@ class EditProfile(QWidget):
         # Window Geometry
         self.setGeometry(100, 200, 1000, 800)
         self.setWindowTitle(f'ConectX Project - {self.username}')
-
+        # 1
         self.logout_button = Login_Button(
             'logoutnButton', (300, 250), 'Close session', self)
         self.logout_button.setCursor(
             QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.logout_button.setStyleSheet(
             button_style)
+        # 2
+        self.logout_button2 = Login_Button(
+            'logoutnButton', (300, 250), 'Close session', self)
+        self.logout_button2.setCursor(
+            QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+        self.logout_button2.setStyleSheet(
+            button_style)
+        # 3
+        self.logout_button3 = Login_Button(
+            'logoutnButton', (300, 250), 'Close session', self)
+        self.logout_button3.setCursor(
+            QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+        self.logout_button3.setStyleSheet(
+            button_style)
+        # 4
+        self.logout_button4 = Login_Button(
+            'logoutnButton', (300, 250), 'Close session', self)
+        self.logout_button4.setCursor(
+            QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+        self.logout_button4.setStyleSheet(
+            button_style)
+        # 5
+        self.logout_button5 = Login_Button(
+            'logoutnButton', (300, 250), 'Close session', self)
+        self.logout_button5.setCursor(
+            QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+        self.logout_button5.setStyleSheet(
+            button_style)
         # self.logout_button.clicked.connect(self.change_username_status)
 
-        # Horizontal Layout
-        hbox1 = QHBoxLayout()
-        hbox1.addWidget(self.logout_button)
-        # Second Horizontal Layout
-        hbox2 = QHBoxLayout()
-        hbox2.addStretch(1)
-        hbox2.addStretch(1)
-        # Third Horizontal Layout
-        hbox3 = QHBoxLayout()
-        hbox3.addStretch(1)
-        hbox3.addStretch(1)
-        # Four
-        hbox4 = QHBoxLayout()
-        hbox4.addStretch(1)
-        # Vertical
-        vbox = QVBoxLayout()
-        vbox.addStretch(2)
-        vbox.addLayout(hbox1)
-        vbox.addLayout(hbox2)
-        vbox.addLayout(hbox3)
-        vbox.addLayout(hbox4)
-        vbox.addStretch(2)
-        vbox.addStretch(5)
-        self.setLayout(vbox)
+        # hbox4.addStretch(1)
+
+        # Horizonatal Layout
+        hbox_left1 = QHBoxLayout()
+        hbox_left1.addWidget(self.logout_button)
+        hbox_left1.addStretch(1)
+        hbox_left2 = QHBoxLayout()
+        hbox_left2.addWidget(self.logout_button2)
+        hbox_left2.addStretch(1)
+        hbox_left3 = QHBoxLayout()
+        hbox_left3.addWidget(self.logout_button3)
+        hbox_left3.addStretch(1)
+        hbox_left4 = QHBoxLayout()
+        hbox_left4.addWidget(self.logout_button4)
+        hbox_left4.addStretch(1)
+        hbox_left5 = QHBoxLayout()
+        hbox_left5.addWidget(self.logout_button5)
+        hbox_left4.addStretch(1)
+        # Left menu
+        vbox_left = QVBoxLayout()
+        vbox_left.addLayout(hbox_left1)
+        vbox_left.addLayout(hbox_left2)
+        vbox_left.addLayout(hbox_left3)
+        vbox_left.addLayout(hbox_left4)
+        vbox_left.addLayout(hbox_left5)
+        vbox_left.addStretch(20)
+        # RIGHT
+        vbox_right = QVBoxLayout()
+        vbox_right.addStretch(20)
+        hbox_principal = QHBoxLayout()
+        hbox_principal.addStretch(20)
+        hbox_principal.addLayout(vbox_right)
+        hbox_principal.addLayout(vbox_left)
+        self.setLayout(hbox_principal)
         # self.show()
